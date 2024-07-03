7fe8da00707d5f51785e40be62ce1d92e4936b30
admin <admin@DESKTOP-V09RESP>
2024-07-03 07:05:14 -0700

    commit message

---
+++ corejavaprogram/src/com/tnsif/scannerclass/Scannerexample.java
@@ -0,0 +1,15 @@
+package com.tnsif.scannerclass;
+
+import java.util.Scanner;
+
+//demo for scanner
+
+public class Scannerexample {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter your name");
+	String name=sc.nextLine();
+	System.out.println("enter the rollnumber");
+	int roll=sc.nextInt();
+}
+}
